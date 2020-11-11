5d45b4c05a94016487c6d4942a10e9b360c0b816
andre ferrer <andrerferrer@hotmail.com>
2020-11-11 08:17:38 -0300

    Finish demo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
-  resources :restaurants
+  
+  # As Mr. Tweeddale asked, we can do this as well.
+  # get 'restaurants/top', to: 'restaurants#top', as: :top_restaurants
+
+  resources :restaurants do
+    # means that this is a route for all restaurants
+    collection do
+      get :top
+    end
+
+    # means that this is a route for one restaurant
+    member do
+      get :chef
+    end
+    
+    resources :reviews, only: [:create, :new]
+  end
+  
+  resources :reviews, only: [:destroy]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", stars: 5 }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", stars: 4 }
+
+[ dishoom, pizza_east ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  # create is going to return true or false
+  # create! is going to return true or the errors
+
+  #   restaurant = Restaurant.new(attributes)
+  #   restaurant.save!
+
+  puts "Created #{restaurant.name}"
+end
+
+puts 'Creating some chefs'
+sebastien = Chef.create(name: 'Sebastien', restaurant: Restaurant.first)
+juan = Chef.create(name: 'Juan', restaurant: Restaurant.second)
+
+puts "Finished!"
